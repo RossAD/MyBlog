8fdefd3ec3d31d7295d3c7661c377d8a7997db79
Ross <ross.ad@gmail.com>
2016-06-06 22:38:05 -0700

    Dropdown nav menu created

--- pages/_template.js
+++ pages/_template.js
@@ -10,42 +10,6 @@ import '../css/styles.css'
 class Template extends React.Component {
   render () {
     const { location, children } = this.props
-    let header
-    if (location.pathname === prefixLink('/')) {
-      header = (
-        <h1
-          style={{
-            fontSize: fontSizeToMS(2.5).fontSize,
-            lineHeight: fontSizeToMS(2.5).lineHeight,
-            marginBottom: rhythm(1.5),
-          }}
-        >
-          <Link
-            style={{
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={prefixLink('/')}
-          >
-            {config.blogTitle}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3>
-          <Link
-            style={{
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={prefixLink('/')}
-          >
-            {config.blogTitle}
-          </Link>
-        </h3>
-      )
-    }
     return (
       <Container
         style={{
